0caea37ccb6156560c608b082867654ac4b5306d
Sravanthi <sravanthi.cb@gmail.com>
2018-10-09 16:26:22 -0400

    Update service-worker-registration.js

--- js/service-worker-registration.js
+++ js/service-worker-registration.js
@@ -1,6 +1,6 @@
 //// Registering service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('FEND-Restaurant-Reviews-App/sw.js', {scope: '/FEND-Restaurant-Reviews-App/'})
+  navigator.serviceWorker.register('/FEND-Restaurant-Reviews-App/sw.js', {scope: '/FEND-Restaurant-Reviews-App/'})
   .then(function(reg) {
     // registration worked
     console.log('Registration succeeded. Scope is ' + reg.scope);
