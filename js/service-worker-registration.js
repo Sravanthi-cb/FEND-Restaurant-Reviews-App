d5b77eb1a579451401d83646466b175db2223f30
Sravanthi <sravanthi.cb@gmail.com>
2018-10-09 15:06:36 -0400

    Update service-worker-registration.js

--- js/service-worker-registration.js
+++ js/service-worker-registration.js
@@ -1,6 +1,6 @@
 //// Registering service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', {scope: 'https://github.com/Sravanthi-cb/FEND-Restaurant-Reviews-App-Stage-1'})
+  navigator.serviceWorker.register('sw.js', {scope: 'https://github.com/Sravanthi-cb/FEND-Restaurant-Reviews-App-Stage-1'})
   .then(function(reg) {
     // registration worked
     console.log('Registration succeeded. Scope is ' + reg.scope);
